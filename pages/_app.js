7d21e09342eb2e9f7e3b2aae17b8fd2243cdf8b7
Borales <bordun.alexandr@gmail.com>
2020-08-10 16:40:47 +0200

    Adding prettier Configuring MDX Provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,19 @@
-import { ZeitProvider, CssBaseline } from '@zeit-ui/react';
+import { ZeitProvider, CssBaseline } from '@zeit-ui/react'
+import { MDXProvider } from '@mdx-js/react'
+import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ZeitProvider>
-      <CssBaseline />
-      <Component {...pageProps} />
+      <MDXProvider
+        components={{
+          wrapper: (props) => <Layout {...props}></Layout>,
+        }}
+      >
+        <CssBaseline />
+        <Component {...pageProps} />
+      </MDXProvider>
     </ZeitProvider>
-  );
+  )
 }
-export default MyApp;
+export default MyApp
