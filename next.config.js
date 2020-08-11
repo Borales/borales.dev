6ef3a56549b0ecefb2ef152188881563b97798c5
Borales <bordun.alexandr@gmail.com>
2020-08-11 23:28:41 +0200

    Enabling next-mdx-enhanced

--- next.config.js
+++ next.config.js
@@ -1,3 +1,25 @@
-module.exports = {
-  target: 'serverless'
-};
+const withMdxEnhanced = require('next-mdx-enhanced')
+
+module.exports = withMdxEnhanced({
+  layoutPath: 'layouts',
+  defaultLayout: true,
+  fileExtensions: ['mdx'],
+  remarkPlugins: [],
+  rehypePlugins: [],
+  extendFrontMatter: {
+    process: (mdxContent, frontMatter) => {},
+    phase: 'prebuild|loader|both',
+  },
+})({
+  target: 'serverless',
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: 'empty',
+      }
+    }
+
+    return config
+  },
+})
--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,10 @@
 import { ZeitProvider, CssBaseline } from '@zeit-ui/react'
-import { MDXProvider } from '@mdx-js/react'
-import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ZeitProvider>
-      <MDXProvider
-        components={{
-          wrapper: (props) => <Layout {...props}></Layout>,
-        }}
-      >
-        <CssBaseline />
-        <Component {...pageProps} />
-      </MDXProvider>
+      <CssBaseline />
+      <Component {...pageProps} />
     </ZeitProvider>
   )
 }
