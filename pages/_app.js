00df90badc50bdc758d2eb9dc6722a8fbd3cf826
Borales <bordun.alexandr@gmail.com>
2020-08-12 18:40:06 +0200

    Adding GAnalytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,21 @@
+import { useEffect } from 'react'
 import Head from 'next/head'
+import Router from 'next/router'
 import { ZeitProvider, CssBaseline } from '@zeit-ui/react'
+import * as gtag from '../lib/gtag'
 import './../styles/global.css'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    Router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      Router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [])
+
   return (
     <ZeitProvider>
       <Head>
--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { Fragment } from 'react'
 import { CssBaseline } from '@zeit-ui/react'
+import { GA_TRACKING_ID } from '../lib/gtag'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -21,7 +22,28 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          {!!GA_TRACKING_ID && (
+            <Fragment>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+                }}
+              />
+            </Fragment>
+          )}
+        </Head>
         <body>
           <Main />
           <NextScript />
