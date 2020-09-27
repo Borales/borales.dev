72783e38e6b556362013f4a30783df0a84362673
Borales <bordun.alexandr@gmail.com>
2020-09-27 20:16:28 +0200

    Updating next and next-mdx-enhanced packages

--- layouts/index.js
+++ layouts/index.js
@@ -5,53 +5,45 @@ import DateFormat from '../components/DateFormat'
 import User from '../components/User'
 import { Breadcrumbs, Spacer, Text, Grid, Note } from '@zeit-ui/react'
 
-export default function BlogLayout(frontMatter) {
-  return ({ children }) => {
-    return (
-      <Layout>
-        <Head>
-          <title>{frontMatter.title} | borales.dev</title>
-          <meta
-            property="article:published_time"
-            content={new Date(frontMatter.publishedAt).toISOString()}
-          />
-        </Head>
-        <Breadcrumbs>
-          <Link href="/">
-            <Breadcrumbs.Item href="/" nextLink>
-              Home
-            </Breadcrumbs.Item>
-          </Link>
-          <Link href="/blog">
-            <Breadcrumbs.Item href="/blog" nextLink>
-              Blog
-            </Breadcrumbs.Item>
-          </Link>
-          <Breadcrumbs.Item>{frontMatter.title}</Breadcrumbs.Item>
-        </Breadcrumbs>
+export default function BlogLayout({ children, frontMatter }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{frontMatter.title} | borales.dev</title>
+        <meta
+          property="article:published_time"
+          content={new Date(frontMatter.publishedAt).toISOString()}
+        />
+      </Head>
+      <Breadcrumbs>
+        <Link href="/">
+          <Breadcrumbs.Item href="/" nextLink>
+            Home
+          </Breadcrumbs.Item>
+        </Link>
+        <Link href="/blog">
+          <Breadcrumbs.Item href="/blog" nextLink>
+            Blog
+          </Breadcrumbs.Item>
+        </Link>
+        <Breadcrumbs.Item>{frontMatter.title}</Breadcrumbs.Item>
+      </Breadcrumbs>
 
-        <h1>{frontMatter.title}</h1>
-        <Grid.Container alignItems="center" justify="space-between">
-          <Grid>
-            <User />
-          </Grid>
-          <Grid>
-            <Text
-              small
-              title={frontMatter.publishedAt}
-              style={{ color: 'grey' }}
-            >
-              <DateFormat date={frontMatter.publishedAt} />
-            </Text>
-          </Grid>
-        </Grid.Container>
-        <Spacer y={1} />
+      <h1>{frontMatter.title}</h1>
+      <Grid.Container alignItems="center" justify="space-between">
+        <Grid>
+          <User />
+        </Grid>
+        <Grid>
+          <Text small title={frontMatter.publishedAt} style={{ color: 'grey' }}>
+            <DateFormat date={frontMatter.publishedAt} />
+          </Text>
+        </Grid>
+      </Grid.Container>
+      <Spacer y={1} />
 
-        {frontMatter.summary && (
-          <Note label={false}>{frontMatter.summary}</Note>
-        )}
-        {children}
-      </Layout>
-    )
-  }
+      {frontMatter.summary && <Note label={false}>{frontMatter.summary}</Note>}
+      {children}
+    </Layout>
+  )
 }
